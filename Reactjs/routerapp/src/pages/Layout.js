04ff617d64cacedf590569faf8508be097bdede2
azartechnet <azartechnet@gmail.com>
2024-12-07 09:44:00 +0530

    updated

--- Reactjs/routerapp/src/pages/Layout.js
+++ Reactjs/routerapp/src/pages/Layout.js
@@ -1,8 +1,10 @@
 import { Outlet,Link } from "react-router-dom";
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const Layout=()=>{
     
     return(
-        <>
+        /*<>
            <nav>
             <ul>
                 <li>
@@ -17,7 +19,28 @@ const Layout=()=>{
             </ul>
            </nav>
            <Outlet/>
-        </>
+        </>*/
+
+        <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">My Website</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
+              <Nav.Link as={Link} to="/Blogs">Blogs</Nav.Link>
+              <Nav.Link as={Link} to="/NoPage">NoPage</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container>
+        <Outlet />
+      </Container>
+    </div>
+   
     )
 }
 
