df1f817ba7446730abdbde0459d1fa88f524ad6a
azartechnet <azartechnet@gmail.com>
2025-03-05 10:00:05 +0530

    updated

---
+++ Reactjs/MySQLCRUD/react-mysql-crud/react-mysql-crud-client/src/CreatePost.js
@@ -0,0 +1,6 @@
+const CreatePost=()=>{
+    return(
+        <h1>This is CreatPost</h1>
+    )
+}
+export default CreatePost;
---
+++ Reactjs/MySQLCRUD/react-mysql-crud/react-mysql-crud-client/src/PostsList.js
@@ -0,0 +1,6 @@
+const PostsList=()=>{
+    return(
+        <h1>This is PostsList</h1>
+    )
+}
+export default PostsList;
