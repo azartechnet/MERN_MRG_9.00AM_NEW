0faf4c584c7d69c96f46ca5f58d970bbe4d858c3
azartechnet <azartechnet@gmail.com>
2025-03-07 10:24:05 +0530

    updated

--- Reactjs/MySQLCRUD/react-mysql-crud/react-mysql-crud-client/src/CreatePost.js
+++ Reactjs/MySQLCRUD/react-mysql-crud/react-mysql-crud-client/src/CreatePost.js
@@ -1,6 +1,53 @@
+import { useState } from "react";
+import {useNavigate} from 'react-router-dom';
+import axios from "axios";
 const CreatePost=()=>{
+
+    const [title,setTitle]=useState("")
+    const [body,setBody]=useState("");
+    const navigate=useNavigate();
+
+    //Data Insereted..
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        axios.post('http://localhost:3001/addpost',{title,body})
+        .then((response)=>{
+            console.log(response.data);
+            navigate('/');
+            })
+            .catch((error)=>{
+                console.log(error);
+                });
+            }
     return(
-        <h1>This is CreatPost</h1>
+       
+        <div className="container mt-5">
+            <h1>This is CreatPost</h1>
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3>Create a NewPost</h3>
+                        </div>
+                        <div className="card-body">
+                             <form onSubmit={handleSubmit}>
+                                 <div className="form-group">
+                                    <label>Title</label>
+                                    <input type="text" className="form-control" placeholder="Enter Title"
+                                    value={title} onChange={(e)=>setTitle(e.target.value)}/>
+                                 </div>
+                                 <div className="form-group">
+                                    <label>Body</label>
+                                    <textarea className="form-control" placeholder="Enter Body" value={body} onChange={(e)=>setBody(e.target.value)}/>
+                                 </div>
+                                 <button type="submit" className="btn btn-primary">Create Post</button>
+                             </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default CreatePost;
---
+++ Reactjs/MySQLCRUD/react-mysql-crud/server.js
@@ -0,0 +1,52 @@
+const express=require('express')
+const mysql=require('mysql')
+const bodyparse=require('body-parser')
+const cors=require('cors')
+
+const app=express()
+const port=3001;
+
+//Middleware
+app.use(cors())
+app.use(bodyparse.json())
+
+//MySQL
+
+const db=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'nishadb'
+    })
+db.connect((err)=>{
+    if(!err)
+        console.log('DB connected')
+    else
+    console.log('DB not connected')
+})
+
+//APIs
+
+//Create table
+
+app.get("/createtable",(req,res)=>{
+    let sql='create table posts(id int auto_increment,title varchar(100),body text,primary key(id))';
+    db.query(sql,(err,results)=>{
+        if(!err)
+            console.log('Table created')
+        res.send('Table created')
+        })
+})
+//Insert data
+app.post("/addpost",(req,res)=>{
+    let post={title:req.body.title,body:req.body.body};
+    let sql='insert into posts SET ?';
+    db.query(sql,post,(err,results)=>{
+        if(!err)
+            console.log('Post added')
+        res.send('Post added')
+        })
+})
+app.listen(port,()=>{
+    console.log(`Server started at port ${port}`)
+})
